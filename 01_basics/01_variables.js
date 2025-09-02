9624cce92a815600df0391c1302e685cb51e2d44
RohitCYadav <rohitcyadav29@gmail.com>
2025-09-02 06:45:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 1234;
+let accountEmail = "rohityadav@gmail.com";
+var accountPassword = "12345"
+accountCity = "kolhapur";
+let userName;
+console.table([accountEmail,accountID,accountPassword,userName])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+/*
+
+number : 2to powerr 53
+bigint 
+string : "abcdefghi"
+boolean : true / false
+null => standalone value
+undefined => 
+symbol = > unique
+data types : 
+*/
+
+console.log(typeof age);
