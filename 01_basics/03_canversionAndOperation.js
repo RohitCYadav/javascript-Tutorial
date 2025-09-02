8c7ac2c919fcd5cef059d29e5b6a211623d4579a
RohitCYadav <rohitcyadav29@gmail.com>
2025-09-02 07:01:26 +0000

    canversion of datatypes

---
+++ 01_basics/03_canversionAndOperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log("--------------------");
+
+let valueInNumber = Number(score)
+
+console.log(typeof (valueInNumber));
+//"33" = > 33
+// "33abc" => NaN
+//true => 1 false => 0
+//"" empty string is false any string is true
+
+console.log("--------------");
+console.log(Boolean(""));
+console.log(Boolean("abc"));
+
